9de6ad5a6837acfefad16965bc553b6273957831
younessgt <youness.merzak123@gmail.com>
2024-01-03 23:44:42 +0100

    task 10

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,18 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default async function asyncUploadUser() {
+  let photo, user;
+  try {
+    photo = await uploadPhoto();
+  } catch (error) {
+    photo = null;
+  }
+
+  try {
+    user = await createUser();
+  } catch (error) {
+    user = null;
+  }
+
+  return { photo, user };
+}
