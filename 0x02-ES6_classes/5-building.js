cccb5891e496240b2a9cdffd35e74b0505c0a1fb
younessgt <youness.merzak123@gmail.com>
2024-01-04 17:15:56 +0100

    task5

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,9 +1,10 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = sqft;
+    
     if (this.constructor !== Building && this.evacuationWarningMessage === undefined) {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
+    this._sqft = sqft;
   }
 
   get sqft() {
