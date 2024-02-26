04c9c93b5449697aeed53409fbb2c0fdc469e279
younessgt <youness.merzak123@gmail.com>
2024-02-26 21:49:05 +0100

    task5

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -8,31 +8,31 @@ const path = process.argv[2];
 
 const app = http.createServer(async (req, res) => {
   const parsedUrl = url.parse(req.url, true);
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
   if (parsedUrl.pathname === '/') {
-    res.write('Hello Holberton School!');
-    res.end();
+    res.end('Hello Holberton School!');
+    return;
   }
 
   if (parsedUrl.pathname === '/students') {
-    await countStudents(path).then((data) => {
-      const { fields, names, length: len } = { ...data };
+    try {
+      const data = await countStudents(path);
+      const { fields, names, length: len } = data;
       let msg2 = '';
       const msg1 = `Number of students: ${len}\n`;
       for (const [key, val] of Object.entries(fields)) {
         const nameString = names[key].join(', ');
         msg2 += `Number of students in ${key}: ${val}. List: ${nameString}\n`;
       }
-      const message = msg1 + msg2;
+      const message = `This is the list of our students\n${msg1}${msg2}`;
       res.writeHead(200, { 'Content-Type': 'text/plain' });
-      res.write('This is the list of our students\n');
       res.end(message.trim());
-    }).catch((error) => {
+    } catch (error) {
       res.end(error.message);
-    });
+    }
+    return;
   }
-  res.statusCode = 404;
-  res.end();
+  res.writeHead(404, { 'Content-Type': 'text/plain' });
+  res.end('Not Found');
 });
 
 app.listen(port);
