870f719879751de292b50f3cad881c61be51676e
younessgt <youness.merzak123@gmail.com>
2024-02-26 18:03:25 +0100

    task 3 second method

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
+/*
+// method 1
 async function countStudents(path) {
   if (await fs.existsSync(path)) {
     const names = {};
@@ -23,6 +25,37 @@ async function countStudents(path) {
   } else {
     throw new Error('Cannot load the database');
   }
+}
+
+module.exports = countStudents;
+*/
+// method 2
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+      }
+      const names = {};
+      const fields = {};
+      const listLines = data.toString().trim().split('\n');
+      console.log(`Number of students: ${listLines.length - 1}`);
+
+      listLines.slice(1).forEach((str) => {
+        const newList = str.split(',');
+        const field = newList.pop();
+        fields[field] = Object.prototype.hasOwnProperty.call(fields, field) ? fields[field] + 1 : 1;
+        names[field] = Object.prototype.hasOwnProperty.call(names, field)
+          ? [...names[field], newList[0]]
+          : [newList[0]];
+      });
+      for (const [key, val] of Object.entries(fields)) {
+        const nameString = names[key].join(', ');
+        console.log(`Number of students in ${key}: ${val}. List: ${nameString}`);
+      }
+      resolve(data);
+    });
+  });
 }
 
 module.exports = countStudents;
