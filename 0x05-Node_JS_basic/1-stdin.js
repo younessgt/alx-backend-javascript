6caf34efdedcf489bc04d34b76337ed1818adad5
younessgt <youness.merzak123@gmail.com>
2024-02-26 22:51:31 +0100

    task1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,17 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (data) => {
-  const name = data.toString().trim();
-  if (!process.stdin.isTTY) {
+const writefunc = () => {
+  process.stdin.on('data', (data) => {
+    const name = data.toString().trim();
     process.stdout.write(`Your name is: ${name}\n`);
-    process.stdout.write('This important software is now closing');
-  } else {
-    process.stdout.write(`Your name is: ${name}`);
-  }
-  process.exit();
-});
+    process.exit();
+  });
+};
+if (process.stdin.isTTY) {
+  writefunc();
+} else {
+  writefunc();
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
+}
