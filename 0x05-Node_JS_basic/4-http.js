ed3afb4e55b05f9fb60849f76902d289a613f4e1
younessgt <youness.merzak123@gmail.com>
2024-02-26 18:28:43 +0100

    task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,1 +1,10 @@
-#!/usr/bin/node
+const http = require('http');
+
+const port = 1245;
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
+});
+
+app.listen(port);
