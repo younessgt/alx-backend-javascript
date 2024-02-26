ea7668691f3bf7b1686df5b1b6d341918f10f604
younessgt <youness.merzak123@gmail.com>
2024-02-26 17:47:29 +0100

    task 3

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,1 +1,28 @@
-#!/usr/bin/node
+const fs = require('fs');
+
+async function countStudents(path) {
+  if (await fs.existsSync(path)) {
+    const names = {};
+    const fields = {};
+    const fileContent = await fs.readFileSync(path, 'utf8');
+    const listLines = fileContent.toString().trim().split('\n');
+    console.log(`Number of students: ${listLines.length - 1}`);
+
+    listLines.slice(1).forEach((str) => {
+      const newList = str.split(',');
+      const field = newList.pop();
+      fields[field] = Object.prototype.hasOwnProperty.call(fields, field) ? fields[field] + 1 : 1;
+      names[field] = Object.prototype.hasOwnProperty.call(names, field)
+        ? [...names[field], newList[0]]
+        : [newList[0]];
+    });
+    for (const [key, val] of Object.entries(fields)) {
+      const nameString = names[key].join(', ');
+      console.log(`Number of students in ${key}: ${val}. List: ${nameString}`);
+    }
+  } else {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
