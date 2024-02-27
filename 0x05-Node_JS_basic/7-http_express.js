2dd9da0a128433eaa75ddeb03429ec5e52781131
younessgt <youness.merzak123@gmail.com>
2024-02-27 16:11:18 +0100

    task7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,1 +1,21 @@
-#!/usr/bin/node
+const express = require('express');
+const countStudents = require('./3-read_file_async');
+
+const database = process.argv[2];
+const app = express();
+const port = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.get('/students', (req, res) => {
+  const msg = 'This is the list of our students\n';
+  countStudents(database).then((data) => {
+    res.send(`${msg}${data.join('\n')}`);
+  }).catch((error) => {
+    res.send(`${msg}${error.message}`);
+  });
+});
+app.listen(port);
+module.exports = app;
