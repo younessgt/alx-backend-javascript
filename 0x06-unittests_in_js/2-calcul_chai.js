9fced6d443b393f6945e062c1ce58afcd70339b9
younessgt <youness.merzak123@gmail.com>
2024-02-28 18:03:28 +0100

    task2

--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,1 +1,30 @@
-#!/usr/bin/node
+function calculateNumber(type, a, b) {
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    throw TypeError('Arg must be numbers');
+  }
+  if (typeof type !== 'string') {
+    throw TypeError('type must be string');
+  }
+  const roundA = Math.round(a);
+  const roundB = Math.round(b);
+  let result;
+  switch (type) {
+    case 'SUM':
+      result = roundA + roundB;
+      break;
+
+    case 'SUBTRACT':
+      result = roundA - roundB;
+      break;
+    case 'DIVIDE':
+      if (roundB === 0) {
+        return 'Error';
+      }
+      result = roundA / roundB;
+      break;
+    default:
+      result = 'type should be SUM or SUBTRACTR or DIVIDE';
+  }
+  return result;
+}
+module.exports = calculateNumber;
--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -1,1 +1,41 @@
-#!/usr/bin/node
+const { expect } = require("chai");
+const calculateNumber = require("./1-calcul");
+
+describe("calculateNumber", () => {
+  it("test SUM", () => {
+    expect(calculateNumber("SUM", 1, 3)).to.equal(4);
+    expect(calculateNumber("SUM", 10, 3)).to.equal(13);
+    expect(calculateNumber("SUM", 0, 5)).to.equal(5);
+    expect(calculateNumber("SUM", 1, -3)).to.equal(-2);
+  });
+
+  it("test SUBTRACT", () => {
+    expect(calculateNumber("SUBTRACT", 1, 3)).to.equal(-2);
+    expect(calculateNumber("SUBTRACT", 1.4, 4.5)).to.equal(-4);
+    expect(calculateNumber("SUBTRACT", 10, 8)).to.equal(2);
+  });
+
+  it("test DIVIDE", () => {
+    expect(calculateNumber("DIVIDE", 10, 2)).to.equal(5);
+    expect(calculateNumber("DIVIDE", 1.4, 4.5)).to.equal(0.2);
+    expect(calculateNumber("DIVIDE", 10, 2.2)).to.equal(5);
+    expect(calculateNumber("DIVIDE", 1.2, 0)).to.equal("Error");
+  });
+
+  it("test not a number", () => {
+    expect(() => calculateNumber("test", 12)).to.throw(
+      TypeError,
+      "Arg must be numbers"
+    );
+    expect(() => calculateNumber(12, "test")).to.throw(
+      TypeError,
+      "Arg must be numbers"
+    );
+  });
+  it("test type not a string", () => {
+    expect(() => calculateNumber(0, 10, 12)).to.throw(
+      TypeError,
+      "type must be string"
+    );
+  });
+});
