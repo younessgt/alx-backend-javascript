61b2d099d84e48ea74d76904842e8c3d875fcf9d
younessgt <youness.merzak123@gmail.com>
2024-02-28 17:43:09 +0100

    task1

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -2,17 +2,29 @@ function calculateNumber(type, a, b) {
   if (typeof a !== 'number' || typeof b !== 'number') {
     throw TypeError('Arg must be numbers');
   }
-  if (typeof type !== 'string'){
+  if (typeof type !== 'string') {
     throw TypeError('type must be string');
   }
+  const roundA = Math.round(a);
+  const roundB = Math.round(b);
+  let result;
   switch (type) {
     case 'SUM':
-      result = a + b;
+      result = roundA + roundB;
       break;
 
     case 'SUBTRACT':
+      result = roundA - roundB;
       break;
+    case 'DIVIDE':
+      if (roundB === 0) {
+        return 'Error';
+      }
+      result = roundA / roundB;
+      break;
+    default:
+      result = 'type should be SUM or SUBTRACTR or DIVIDE';
   }
-  return Math.round(a + b);
+  return result;
 }
 module.exports = calculateNumber;
--- 0x06-unittests_in_js/1-calcul.test.js
+++ 0x06-unittests_in_js/1-calcul.test.js
@@ -1,1 +1,41 @@
-#!/usr/bin/node
+const assert = require('assert');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+  it('test SUM', () => {
+    assert.strictEqual(calculateNumber('SUM', 1, 3), 4);
+    assert.strictEqual(calculateNumber('SUM', 10, 3), 13);
+    assert.strictEqual(calculateNumber('SUM', 0, 5), 5);
+    assert.strictEqual(calculateNumber('SUM', 1, -3), -2);
+  });
+
+  it('test SUBTRACT', () => {
+    assert.strictEqual(calculateNumber('SUBTRACT', 1, 3), -2);
+    assert.strictEqual(calculateNumber('SUBTRACT', 1.4, 4.5), -4);
+    assert.strictEqual(calculateNumber('SUBTRACT', 10, 8), 2);
+  });
+
+  it('test DIVIDE', () => {
+    assert.strictEqual(calculateNumber('DIVIDE', 10, 2), 5);
+    assert.strictEqual(calculateNumber('DIVIDE', 1.4, 4.5), 0.2);
+    assert.strictEqual(calculateNumber('DIVIDE', 10, 2.2), 5);
+    assert.strictEqual(calculateNumber('DIVIDE', 1.2, 0), 'Error');
+  });
+
+  it('test not a number', () => {
+    assert.throws(() => calculateNumber('test', 12), {
+      name: 'TypeError',
+      message: 'Arg must be numbers',
+    });
+    assert.throws(() => calculateNumber(12, 'test'), {
+      name: 'TypeError',
+      message: 'Arg must be numbers',
+    });
+  });
+  it('test type not a string', () => {
+    assert.throws(() => calculateNumber(0 ,10, 12), {
+      name: 'TypeError',
+      message: 'type must be string',
+    });
+  });
+});
