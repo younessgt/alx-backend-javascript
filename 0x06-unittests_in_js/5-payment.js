f15b80ad478b07e3d3771007ea601af7da69d319
younessgt <youness.merzak123@gmail.com>
2024-02-28 21:12:10 +0100

    test 5

--- 0x06-unittests_in_js/5-payment.js
+++ 0x06-unittests_in_js/5-payment.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+const Utils = require('./utils');
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  const total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${total}`);
+  return total;
+}
+
+module.exports = sendPaymentRequestToApi;
--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -1,1 +1,25 @@
-#!/usr/bin/node
+const sinon = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToAPI = require('./5-payment');
+
+describe('test with hooks function', () => {
+  let consoleLogSpy;
+
+  beforeEach(() => {
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+  afterEach(() => {
+    consoleLogSpy.restore();
+  });
+
+  it('should return The Total is 20', () => {
+    sendPaymentRequestToAPI(10, 10);
+    expect(consoleLogSpy.calledOnce).to.equal(true);
+    expect(consoleLogSpy.calledWithExactly('The total is: 20')).to.equal(true);
+  });
+  it('should return The Total is 120', () => {
+    sendPaymentRequestToAPI(100, 20);
+    expect(consoleLogSpy.calledOnce).to.equal(true);
+    expect(consoleLogSpy.calledWithExactly('The total is: 120')).to.equal(true);
+  });
+});
