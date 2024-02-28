82458cbb3119b93a7c38b00a2ec1d4eb5d85f535
younessgt <youness.merzak123@gmail.com>
2024-02-28 20:53:40 +0100

    task4

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,1 +1,21 @@
-#!/usr/bin/node
+const sendPaymentRequestToApi = require('./4-payment');
+const sinon = require('sinon');
+const { expect } = require('chai');
+const Utils = require('./utils');
+
+describe('test sendPaymentRequestToApi', () => {
+  it('Should test calculNumber function', () => {
+    const wrappedCalculNumber = sinon.stub(Utils, 'calculateNumber');
+    wrappedCalculNumber.returns(10);
+    const wrappedConsoleLog = sinon.spy(console, 'log');
+
+    const x = sendPaymentRequestToApi(100, 20);
+    expect(wrappedCalculNumber.calledOnce).to.equal(true);
+    expect(wrappedCalculNumber.calledWithExactly('SUM', 100, 20)).to.equal(true);
+    expect(wrappedConsoleLog.calledWithExactly('The total is: 10')).to.equal(true);
+    expect(x).to.equal(10);
+
+    wrappedCalculNumber.restore();
+    wrappedConsoleLog.restore();
+  });
+});
