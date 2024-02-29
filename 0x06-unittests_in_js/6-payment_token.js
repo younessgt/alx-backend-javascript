c24384e5d46c8b48d89d4fab9f253ea8714e4666
younessgt <youness.merzak123@gmail.com>
2024-02-29 17:23:11 +0100

    task6

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+function getPaymentTokenFromAPI(success) {
+  return new Promise((resolve) => {
+    if (success) {
+      const data = { data: 'Successful response from the API' };
+      resolve(data);
+    }
+  });
+}
+module.exports = getPaymentTokenFromAPI;
--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+const getPaymentTokenFromAPI = require('./6-payment_token');
+const { expect } = require('chai');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('should return object', () => {
+    getPaymentTokenFromAPI(true).then((result) => {
+      expect(result).to.deep.equal({ data: 'Successful response from the API' });
+      done();
+    }).catch((err) => {
+      done(err);
+    });
+  });
+});
