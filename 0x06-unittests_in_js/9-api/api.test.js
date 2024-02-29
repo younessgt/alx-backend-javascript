fa5cd9d4b1a617409925408bc1427236d517e477
younessgt <youness.merzak123@gmail.com>
2024-02-29 19:13:25 +0100

    task9

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -2,6 +2,15 @@ const { expect } = require('chai');
 const request = require('request');
 
 describe('test server', () => {
+  it('should return the correct status code and response', (done) => {
+    request('http://localhost:7865', (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+  
+
   it('should return status code 200 and response', (done) => {
     request('http://localhost:7865/cart/12', (err, res, body) => {
       expect(res.statusCode).to.equal(200);
