75ccfd408a7574dcca3d3029b975c04dfa702a81
younessgt <youness.merzak123@gmail.com>
2024-02-29 17:34:46 +0100

    task6

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,14 +1,12 @@
 const { expect } = require('chai');
 const getPaymentTokenFromAPI = require('./6-payment_token');
 
-
 describe('getPaymentTokenFromAPI', () => {
   it('should return object', () => {
-    getPaymentTokenFromAPI(true).then((result) => {
-      expect(result).to.deep.equal({ data: 'Successful response from the API' });
-      done();
-    }).catch((err) => {
-      done(err);
+    getPaymentTokenFromAPI(true)
+      .then((result) => {
+        expect(result).to.deep.equal({ data: 'Successful response from the API' });
+        done();
     });
   });
 });
