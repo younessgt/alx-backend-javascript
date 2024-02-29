5bfc3bebae782344ce756b9f07bb277bc2ae9511
younessgt <youness.merzak123@gmail.com>
2024-02-29 18:52:38 +0100

    task9

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,1 +1,15 @@
-#!/usr/bin/node
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const cartId = req.params.id;
+  res.send(`Payment methods for cart ${cartId}`);
+});
+app.listen(7865, () => {
+  console.log('API available on localhost port 7865');
+});
+module.exports = app;
--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,1 +1,19 @@
-#!/usr/bin/node
+const { expect } = require('chai');
+const request = require('request');
+
+describe('test server', () => {
+  it('should return status code 200 and response', (done) => {
+    request('http://localhost:7865/cart/12', (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 12');
+      done();
+    });
+  });
+
+  it('should return  status 404', (done) => {
+    request('http://localhost:7865/cart/hello', (err, res) => {
+      expect(res.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
